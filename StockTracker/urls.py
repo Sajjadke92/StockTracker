a66ae266e0006a721fe13013b7f105d0428013ba
Sajjadke92 <sajjadkeykha92@gmail.com>
2024-12-16 18:25:58 +0100

    OOPs & inheritance

--- StockTracker/urls.py
+++ StockTracker/urls.py
@@ -17,11 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from warehouses.views import home, Category_list,Category_detail,Category_create
+
+
+from warehouses.views import home,Category_detail,Category_create,category_list_view
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("home/",home),
     path('warehouses/create/',Category_create, name='Category_create'),
-    path('warehouses/',Category_list,name='Category_list'),
+    path('warehouses/',category_list_view,name='Category_list'),
     path('warehouses/<int:Category_id>/',Category_detail,name='Category_detail')
 ]
--- warehouses/views.py
+++ warehouses/views.py
@@ -1,18 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
-
+from django.http import HttpResponse,HttpResponseRedirect,HttpResponseNotFound,HttpRequest
 
 from .models import Category,Item
 from .forms import CategoryForm
 
 
-def Category_list(request):
-    warehouses = Category.objects.all()
-    context = {'warehouses': warehouses}
-    return render(request, 'warehouses/Category_list.html',context = context)
+class GeneralView:
+    def __init__(self, queryset, template_name, context_object_name):
+        self.queryset = queryset
+        self.template_name = template_name
+        self.context_object_name = context_object_name
+
+    def as_view(self):
+        """Returns a callable function that Django can use as a view."""
+        def view_function(request: HttpRequest):
+            context = {self.context_object_name: self.queryset}
+            return render(request, self.template_name, context)
+        return view_function
+    
+
+class Categorylist(GeneralView):
+    def __init__(self):
+        super().__init__(
+    queryset = Category.objects.all(),
+    template_name = 'warehouses/Category_list.html',
+    context_object_name = 'warehouses'        
+    )
+        
+category_list_view = Categorylist().as_view()
+
+
 
 def Category_detail(request,Category_id):
-    warehouses = Category.objects.get(pk=Category_id)
+    try:
+        warehouses = Category.objects.get(pk=Category_id)
+    except Category.DoesNotExist:
+        return HttpResponseNotFound()  
     items = Item.objects.filter(Category = warehouses)
     context = {'warehouses':warehouses,'items':items}
     return render(request,'warehouses/Category_detail.html',context=context)
